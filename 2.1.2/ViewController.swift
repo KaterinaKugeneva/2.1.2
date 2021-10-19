d1143b370f8ef083bb606d4243bb97bb3ee7c427
KaterinaKugeneva <kugeneva1@gmail.com>
2021-10-19 21:11:59 +0300

    Initial Commit

---
+++ 2.1.2/ViewController.swift
@@ -0,0 +1,54 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var redLight: UIView!
+    
+    @IBOutlet weak var yellowLight: UIView!
+    
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    var state: Int = 0
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        redLight.layer.cornerRadius = 75
+        yellowLight.layer.cornerRadius = 75
+        greenLight.layer.cornerRadius = 75
+    }
+   
+    @IBAction func pressStartButton() {
+        state += 1
+        startButton.setTitle("NEXT", for: .normal)
+        switch state {
+        case 1: do {
+            redLight.alpha = 1
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+        }
+        case 2: do {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+            greenLight.alpha = 0.3
+        }
+        case 3: do {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+            state = 0
+        }
+        default:
+            redLight.alpha = 1
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+        }
+    }
+    
+        
+       
+    
+
+}
