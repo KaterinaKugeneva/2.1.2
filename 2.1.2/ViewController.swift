8d39ad167afae59cd13fb6f47b9c4262ffe372c3
KaterinaKugeneva <kugeneva1@gmail.com>
2021-10-20 10:55:08 +0300

    font size

--- 2.1.2/ViewController.swift
+++ 2.1.2/ViewController.swift
@@ -10,22 +10,25 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var startButton: UIButton!
     var state: Int = 0
-    
-    
-    
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redLight.layer.cornerRadius = redLight.frame.width / 2
-        yellowLight.layer.cornerRadius = redLight.frame.width / 2
-        greenLight.layer.cornerRadius = redLight.frame.width / 2
+        //redLight.layer.cornerRadius = redLight.frame.width / 2
+        //yellowLight.layer.cornerRadius = redLight.frame.width / 2
+        //greenLight.layer.cornerRadius = redLight.frame.width / 2
     }
-   
+    override func viewWillLayoutSubviews() {
+            redLight.layer.cornerRadius = redLight.frame.width / 2
+            yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+            greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        }
    
     @IBAction func pressStartButton() {
         state += 1
         startButton.setTitle("NEXT", for: .normal)
+        
         switch state {
         case 1: do {
             redLight.alpha = 1
